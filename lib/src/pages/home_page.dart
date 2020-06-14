aa2dc138fc2f7d42ac390d6eec2c49efa8f266ee
Sebastian <sanabal@alumnos.ubiobio.cl>
2020-06-13 23:44:04 -0400

    swiper flutter

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 
 class HomePage extends StatelessWidget {
@@ -7,11 +9,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-            child: Text('hola mundo')
-            )
+      appBar: AppBar(
+        title: Text('Peliculas en el cine'),
+        backgroundColor:  Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: (){} )
+
+        ],
+
+      ),
+      body: Container(
+            child: Column(
+              children: <Widget>[
+                _swiperTarjetas(),
+              ],
+            ),
+
+      )
       
       
+      
+    );
+  }
+
+  Widget _swiperTarjetas() {
+
+    return Container(
+        padding: EdgeInsets.only(top:10.0),
+        width: double.infinity,
+        height: 300.0,
+        child:Swiper(
+            layout: SwiperLayout.STACK,
+            itemWidth: 200.0,
+            itemBuilder: (BuildContext context,int index){
+              return new Image.network("http://via.placeholder.com/350x150",
+              fit: BoxFit.fill,);
+            },
+            itemCount: 3,
+           /*  pagination: new SwiperPagination(),
+            control: new SwiperControl(), */
+          ),
       
     );
   }
